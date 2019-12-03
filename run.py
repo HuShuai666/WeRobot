20a4008754db41ae9c6775532db18aed10c3fe4c
HuShuai666 <2313153604@qq.com>
2019-12-03 14:53:00 +0800

    修改启动文件

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import sys
 
 from werobot.contrib.tornado import make_handler
-
+from tornado.options import define, options, parse_command_line
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
@@ -15,6 +15,7 @@ from src.robot import robot
 #     if len(sys.argv) > 1:
 #         port = sys.argv[1]
 #     robot.run(server='tornado', host='0.0.0.0', port=port)
+define('port', default=6060, type=int)
 
 application = tornado.web.Application([
     (r'/weixin/', make_handler(robot)),
@@ -22,12 +23,16 @@ application = tornado.web.Application([
 ])
 
 if __name__ == '__main__':
-    port = 6060
-    if len(sys.argv) > 1:
-        port = sys.argv[1]
-    sockets = tornado.netutil.bind_sockets(int(port))
-    tornado.process.fork_processes(5)
-    http_server = tornado.httpserver.HTTPServer(application)
-    # application.listen(port)
-    http_server.add_sockets(sockets)
-    tornado.ioloop.IOLoop.instance().start()
+    # port = 6060
+    # if len(sys.argv) > 1:
+    #     port = sys.argv[1]
+    # sockets = tornado.netutil.bind_sockets(int(port))
+    # tornado.process.fork_processes(5)
+    # http_server = tornado.httpserver.HTTPServer(application)
+    # # application.listen(port)
+    # http_server.add_sockets(sockets)
+    # tornado.ioloop.IOLoop.instance().start()
+    parse_command_line()
+    app = application
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
