6f6f5d84aa112ae74acd34bea68f107f3cc86c37
HuShuai666 <2313153604@qq.com>
2020-03-09 20:07:48 +0800

    修改启动文件

--- src/functions.py
+++ src/functions.py
@@ -50,17 +50,17 @@ def handle_text_message(message):
             return """创建菜单失败"""
         else:
             return """创建成功"""
-    if content.strip() == '测试回复':
-        count = len(list(quality_questions))
-        answer = 1
-        WeiXin_Server.send_text_message(openid, quality_questions.get(answer))
-        while answer <= count:
-            if content.strip() in ['A', 'a', 'B', 'b', 'C', 'c']:
-                answer += 1
-            else:
-                WeiXin_Server.send_text_message(openid, """哦豁，Nelly识别不了你的答案，请输入题目中包含答案对应的序号，如“A、B、C”""")
-            WeiXin_Server.send_text_message(openid, """恭喜你，已经完成答题""")
-        WeiXin_Server.send_text_message(openid, """稍等片刻，正在为你揭晓答案。。。""")
+    # if content.strip() == '测试回复':
+    #     count = len(list(quality_questions))
+    #     answer = 1
+    #     WeiXin_Server.send_text_message(openid, quality_questions.get(answer))
+    #     while answer <= count:
+    #         if content.strip() in ['A', 'a', 'B', 'b', 'C', 'c']:
+    #             answer += 1
+    #         else:
+    #             WeiXin_Server.send_text_message(openid, """哦豁，Nelly识别不了你的答案，请输入题目中包含答案对应的序号，如“A、B、C”""")
+    #         WeiXin_Server.send_text_message(openid, """恭喜你，已经完成答题""")
+    #     WeiXin_Server.send_text_message(openid, """稍等片刻，正在为你揭晓答案。。。""")
     return
 
 
