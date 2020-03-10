2eaf56e3fe3fc2c959dd5aa82b676e3a50c9ec48
HuShuai666 <2313153604@qq.com>
2020-03-10 23:50:52 +0800

    修改启动文件

--- src/function_task.py
+++ src/function_task.py
@@ -8,7 +8,7 @@ import json
 def quality_test(content, openid):
     if content.strip() == "重新测试":
         redis_client.delete('hu_cs')
-        return
+        return """redis已经删除"""
     WeiXin_Server.send_text_message(openid, """1""")
     if redis_client.hget('hu_cs', openid):
         WeiXin_Server.send_text_message(openid, """2""")
